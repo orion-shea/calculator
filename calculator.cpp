4d5c839e0a7ec168177243dd816d26b492da1d4d
orion-shea <orionashea@gmail.com>
2026-06-27 20:54:16 +0700

    updated math functions

--- calculator.cpp
+++ calculator.cpp
@@ -2,14 +2,33 @@
 
 using namespace std;
 
-int calculator(int a, int b) {
+double addition(double a, double b) {
     return a + b;
 }
 
+double subtraction(double a, double b) {
+    return a - b;
+}
+
+double multiplication(double a, double b) {
+    return a * b;
+}
+
+double division(double a, double b) {
+    if (b == 0) {
+        cout << "Error: Division by zero!" << endl;
+        return 0; // Return 0 or handle the error as needed
+    }
+    return a / b;
+}
+
 int main() {
-    int a, b;
+    double a, b;
     cout << "Enter two numbers: ";
     cin >> a >> b;
-    cout << "The sum is: " << calculator(a, b) << endl;
+    cout << "The sum is: " << addition(a, b) << endl;
+    cout << "The difference is: " << subtraction(a, b) << endl;
+    cout << "The product is: " << multiplication(a, b) << endl;
+    cout << "The quotient is: " << division(a, b) << endl;
     return 0;
 }
