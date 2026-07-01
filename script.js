54e868a735d649a8af8a2a56829fa710fd5b0105
orion-shea <orionashea@gmail.com>
2026-07-02 00:11:08 +0700

    Added functionality to clear and backspacem + some other minor changes.

--- script.js
+++ script.js
@@ -1,13 +1,72 @@
 /*
-const allBtns = document.querySelectorAll('.button-grid button');
+const allBtns = document.querySelectorAll('.calculator-buttons button');
 for(let i = 0; i < allBtns.length; i++) {
     console.log(allBtns[i].id, allBtns[i].value);
 }
 */
+//document.querySelector('.calculator-buttons').addEventListener('click', f);
 document.addEventListener('click', f);
+document.addEventListener('keydown', f);
+
+let acc = 0;
+document.getElementById('display').innerHTML = acc;
 
 function f(event) {
-    console.log(event.target);
-    console.log(event.target.id);
-    console.log(event.target.value);
+    console.log(event);
+
+    let variable;
+    if(event.type === 'click') {
+        variable = event.target.value;
+    }
+    if(event.type === 'keydown') {
+        variable = event.key;
+    }
+
+    console.log(variable);
+    
+    if(variable === '+') {
+        console.log('add');
+        document.getElementById('display').innerHTML += ' + ';
+    }
+    else if(variable === '-') {
+        console.log('subtract');
+        document.getElementById('display').innerHTML += ' - '; 
+    } 
+    else if(variable === '*') {
+        console.log('multiply'); 
+    } 
+    else if(variable === '/') {
+        console.log('divide');
+    } 
+    else if(variable === '=') {
+        console.log('equals');
+    }
+    else if(variable === 'c' || variable === 'C') {
+        console.log('clear');
+        acc = 0;
+        document.getElementById('display').innerHTML = acc;
+    }
+    else if(variable === 'Backspace') {
+        console.log('backspace: ' + acc);
+        if(acc.length === 1) {
+            acc = 0;
+        }
+        else {
+            acc = acc.toString().slice(0, -1);
+        }
+        document.getElementById('display').innerHTML = acc;
+    } 
+    else {
+        if(!isNaN(variable)) {
+            if(document.getElementById('display').innerHTML === '0') {
+                acc = variable;
+                document.getElementById('display').innerHTML = variable;
+            }
+            else {
+                acc += variable;
+                document.getElementById('display').innerHTML += variable;
+            }
+            
+        }
+    }
 }
